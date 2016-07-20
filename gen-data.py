47a4988b9cae1beab805055c088ed7c680763dc9
Evert Lammerts <evert@lucipher.net>
2016-07-20 21:24:55 +0200

    many invalid trips was not a bug

--- gen-data.py
+++ gen-data.py
@@ -20,7 +20,7 @@ with open('test-shifts.txt', 'w') as shift_file:
             trip = {}
             trip['uuid'] = str(uuid.uuid4())
             shift = shifts[random.randint(0, len(shifts) - 1)]
-            if random.randint(0,1000) != 1:
+            if random.randint(0,1000) == 1:
                 trip['driverId'] = shift['driverId']
                 trip['start'] = (shift['start'] + shift['end']) // 2
                 trip['end'] = shift['end']
